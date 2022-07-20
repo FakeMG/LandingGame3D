f93833cb4e22a2306313757c4f3311886b6f1db2
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2022-07-20 23:19:57 +0700

    feat: add parralax effect to background

--- Serious game/Assets/Script/BGScrolling.cs
+++ Serious game/Assets/Script/BGScrolling.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
 public class BGScrolling : MonoBehaviour {
-    [SerializeField] private float speed = .5f;
+    [SerializeField] private float parralax = 2f;
+    [SerializeField] private float speed = 2f;
     private Material material;
 
     // Start is called before the first frame update
@@ -11,8 +12,21 @@ public class BGScrolling : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 30);
+
+        MoveAlongXAxis();
+        AddParralaxEffectOnYAxis();
+    }
+
+    private void MoveAlongXAxis() {
         Vector2 offset = material.mainTextureOffset;
         offset.x += speed * Time.deltaTime;
+        material.mainTextureOffset = offset;
+    }
+
+    private void AddParralaxEffectOnYAxis() {
+        Vector2 offset = material.mainTextureOffset;
+        offset.y = ((transform.position.y / transform.localScale.y) * material.mainTextureScale.y) / parralax;
         material.mainTextureOffset = offset;
     }
 }
